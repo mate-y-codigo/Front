d2cca0df639b3a6a156693f40936822e7bb1739f
Iván Sierra <sierra.ivan@gmail.com>
2025-11-28 11:26:02 -0300

    se agregaron URL

---
+++ src/config/urlApi.js
@@ -0,0 +1,10 @@
+const urlUserApi = 'https://localhost:7211'
+const urlPlanApi = 'https://localhost:7243'
+
+export function getUrlUserApi() {
+    return urlUserApi;
+}
+
+export function getUrlPlanApi() {
+    return urlPlanApi;
+}
