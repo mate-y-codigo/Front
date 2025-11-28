b4ee95c08d29ecde1e2495bcdfaf49885ca6428e
Iván Sierra <sierra.ivan@gmail.com>
2025-11-28 11:24:39 -0300

    se agregaron funcionalidades al combobox

--- src/components/comboBoxHtml.js
+++ src/components/comboBoxHtml.js
@@ -1,24 +1,17 @@
-
-function comboBoxList(text){
-    return `
-        <li data-value="${text}">${text}</li>
-    `;
-}
-
-export function comboBoxHTML(list) {
+export function comboBoxHtml(list) {
     return `
         <div class="combobox">
             <button id="dropdown-button">
-                <span id="selected-option">${list[0]}</span>
+                <span id="selected-option">Seleccione una opción...</span>
                 <span class="material-symbols-outlined transition-transform duration-300" id="arrow-icon">keyboard_arrow_down</span>
             </button>
 
             <div id="dropdown-menu"
                 class="transform scale-y-0 opacity-0 origin-top transition duration-300 ease-out pointer-events-none">
                 <ul class="scroll-rounded">
-                    ${list.map((list) => comboBoxList(list)).join('')}
+                    ${list.map(item => `<li data-id="${item.id}" data-name="${item.name}">${item.name}</li>`).join("")}
                 </ul>
             </div>
         </div>
-    `;    
+    `;
 }
